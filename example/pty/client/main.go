2574d782b316d081aa26158dbabd9ba2a83cea02
Vedant Kulkarni <vedantkulkarni@Vedants-MacBook-Air.local>
2024-12-27 01:38:24 +0530

    feat: add pty example using net bridge

---
+++ example/pty/client/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	bridge "github.com/golain-io/mqtt-bridge"
+	"github.com/google/uuid"
+)
+
+func main() {
+	// Setup MQTT client
+	client := mqtt.NewClient(mqtt.NewClientOptions().
+		AddBroker("tcp://localhost:1883").
+		SetClientID("client-" + uuid.New().String()))
+
+	if ok := client.Connect().Wait(); !ok {
+		log.Fatal("MQTT connect failed")
+	}
+	defer client.Disconnect(0)
+
+	// Setup bridge connection
+	b := bridge.NewMQTTNetBridge(client, "test-client")
+	defer b.Close()
+
+	conn, err := b.Dial(context.Background(), "test-server")
+	if err != nil {
+		log.Fatal("Dial failed:", err)
+	}
+	defer conn.Close()
+
+	errCh := make(chan error, 2)
+
+	// Start goroutine to relay data from connection to stdout
+	go relay(conn, os.Stdout, errCh, "read")
+
+	// Start goroutine to relay data from stdin to connection
+	go relay(os.Stdin, conn, errCh, "write")
+
+	if err := <-errCh; err != nil {
+		log.Fatal(err)
+	}
+}
+
+func relay(r io.Reader, w io.Writer, errCh chan error, op string) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := r.Read(buf)
+		if err != nil {
+			errCh <- fmt.Errorf("%s error: %w", op, err)
+			return
+		}
+		if _, err := w.Write(buf[:n]); err != nil {
+			errCh <- fmt.Errorf("%s write error: %w", op, err)
+			return
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/golain-io/mqtt-bridge
 go 1.23.2
 
 require (
+	github.com/creack/pty v1.1.24
 	github.com/eclipse/paho.mqtt.golang v1.5.0
 	github.com/google/uuid v1.6.0
 	github.com/stretchr/testify v1.9.0
 	go.uber.org/zap v1.27.0
+	golang.org/x/term v0.27.0
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1
 	google.golang.org/grpc v1.68.0
 	google.golang.org/protobuf v1.35.1
@@ -18,8 +20,8 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
